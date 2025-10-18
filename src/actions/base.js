819b07b535a143a854dffec2e7eb1bacc313995e
eelco <git@eelcoj.com>
2025-10-18 14:55:34 +0700

    Added support to cycle through values (using new cycle* action)
    ```html
    <button data-action="cycleAttribute#type=password,text" data-target="#password">
      View password
    </button>
    
    <input type="password" id="password" value="my secret" class="name">
    ```

--- src/actions/base.js
+++ src/actions/base.js
@@ -10,4 +10,12 @@ export default class ActionBase {
   get targets() {
     return this.targetElement ? Array.from(document.querySelectorAll(this.targetElement)) : [this.currentElement];
   }
+
+  cycledValue(currentValue, nextValues) {
+    const values = Array.isArray(nextValues) ? nextValues : nextValues.split(",").map(value => value.trim());
+    const currentIndex = values.indexOf(currentValue);
+    const nextIndex = (currentIndex + 1) % values.length;
+
+    return values[nextIndex];
+  }
 }
--- src/actions/class.js
+++ src/actions/class.js
@@ -14,6 +14,12 @@ class Class extends ActionBase {
     this.targets.forEach(target => this.#toggleClasses({ forEach: target }));
   }
 
+  cycle() {
+    if (!this.value || this.value.length === 0) return;
+
+    this.targets.forEach(target => this.#cycleClasses(target));
+  }
+
   add() {
     if (!this.value) return;
 
@@ -31,6 +37,14 @@ class Class extends ActionBase {
   #toggleClasses({ forEach: target }) {
     this.value.forEach(className => target.classList.toggle(className));
   }
+
+  #cycleClasses(target) {
+    const currentClass = this.value.find(className => target.classList.contains(className)) || "";
+    const nextClass = this.cycledValue(currentClass, this.value);
+
+    target.classList.remove(...this.value);
+    target.classList.add(nextClass);
+  }
 }
 
 export const action = (method) =>
@@ -42,6 +56,7 @@ export const action = (method) =>
 
 export default {
   toggleClass: action("toggle"),
+  cycleClass: action("cycle"),
   addClass: action("add"),
   removeClass: action("remove")
 };
